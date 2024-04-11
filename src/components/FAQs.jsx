0104fe3522d49c5cd2473fd09f381898ac794d40
Sarah Olson <142362888+sarahlynnx@users.noreply.github.com>
2024-04-11 13:28:39 -0700

    Updated path for gh pages

--- src/components/FAQs.jsx
+++ src/components/FAQs.jsx
@@ -24,7 +24,7 @@ const FAQs = () => {
         // Fetching FAQ data from JSON file
         const fetchData = async () => {
             try {
-                const response = await fetch('/data/faqs.json', {
+                const response = await fetch('ProductLandingPage/data/faqs.json', {
                     headers: {
                         'Accept': 'application/json'
                     }
